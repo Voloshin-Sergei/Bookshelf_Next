ea3db92660827c28d0ded43c21fb9c096de6f486
Voloshin_Sergey <4voloshin@gmail.com>
2022-01-12 08:00:29 +0300

    refactor: changed color variables and Button and Headling components

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,7 +1,7 @@
 import { ButtonHTMLAttributes, DetailedHTMLProps, FC, ReactNode } from 'react';
 
 export interface ButtonProps {
-  color: 'success-c' | 'error-c' | 'info-c';
+  color: string;
   type?: 'button' | 'submit';
   text: string;
   icon: JSX.Element;
@@ -11,7 +11,7 @@ export interface ButtonProps {
 export const Button: FC<ButtonProps> = ({ color, type = 'button', text, icon, onclick }) => {
   return (
     <button
-      className={`bg-${color} text-primary-c text-base flex items-center py-0.5 px-3 rounded-full uppercase  hover:opacity-90`}
+      className={`${color} text-primary-c text-base flex items-center py-0.5 px-3 rounded-full uppercase  hover:opacity-90`}
       type={type}>
       {text}
       <span className="ml-2 h-4 w-4">{icon}</span>
--- components/Heading/Heading.tsx
+++ components/Heading/Heading.tsx
@@ -3,16 +3,17 @@ import { FC } from 'react';
 interface HeadingProps {
   tag: 'h1' | 'h2' | 'h3';
   text: string;
+  color: string;
 }
 
-export const Heading: FC<HeadingProps> = ({ tag, text }) => {
+export const Heading: FC<HeadingProps> = ({ tag, text, color }) => {
   switch (tag) {
     case 'h1':
-      return <h1 className="text-4xl font-bold">{text}</h1>;
+      return <h1 className={`${color} text-4xl font-bold `}>{text}</h1>;
     case 'h2':
-      return <h2 className="text-3xl font-bold">{text}</h2>;
+      return <h2 className={`${color} text-3xl font-bold `}>{text}</h2>;
     case 'h3':
-      return <h3 className="text-2xl font-bold">{text}</h3>;
+      return <h3 className={`${color} text-2xl font-bold `}>{text}</h3>;
     default:
       return <></>;
   }
