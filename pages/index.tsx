742a4c06e5fe7019e0e9c64b6ca7b0ab37064c18
Voloshin_Sergey <4voloshin@gmail.com>
2022-01-24 23:55:36 +0300

    feat: added request for mockapi service,fetch all books

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,22 @@
-import type { NextPage } from 'next';
+import axios from 'axios';
+import { BlockList } from 'net';
+import { InferGetServerSidePropsType } from 'next';
+import { BookItemProps } from '../components/BookItem/BookItem';
+import { BooksList } from '../components/BooksList';
 
-const Home: NextPage = () => {
-  return <></>;
+export const getServerSideProps = async () => {
+  const res = await axios.get('https://61b18d0e3c954f001722a9f3.mockapi.io/books');
+  const data: BookItemProps[] = res.data;
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+const Home = ({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  return <BooksList books={data} />;
 };
 
 export default Home;
